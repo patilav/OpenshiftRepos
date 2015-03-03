b0c58b997ba2a64e9f43124781e58dc4bc4dc83c
patilav <patil.av@husky.neu.edu>
2015-03-03 02:39:49 -0500

    Update with the http resposnse in the page

--- public/js/CMApp.js
+++ public/js/CMApp.js
@@ -1,5 +1,7 @@
 ﻿var app = angular.module("CMApp", []);
 
-app.controller("CMController", function ($scope) {
-    $scope.hello = "Hello from CMController";
-})
+app.controller("CMController", function ($scope, $http) {
+    $http.get("/api/website").success(function (response) {
+        $scope.websites = response;
+    });
+});
